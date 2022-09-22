d779f25e92c50cdb8d7d6750beb38cf0ef7e4caf
emerymastering <emery@mastering.lt>
2022-09-22 15:25:14 +0200

    detail page shows details

--- src/pages/Space.js
+++ src/pages/Space.js
@@ -16,9 +16,30 @@ export default function Spaces() {
   console.log("selector spaces", space);
 
   return (
-    <div className="space">
-      <h2>{space.title}</h2>
-      <p>{space.description}</p>
-    </div>
+    <>
+      {space ? (
+        <div>
+          <div
+            className="space"
+            style={{ backgroundColor: `${space.backgroundColor}` }}
+          >
+            <h2 style={{ color: `${space.color}` }}>{space.title}</h2>
+            <p style={{ color: `${space.color}` }}>{space.description}</p>
+          </div>
+          <div>
+            {space.stories &&
+              space.stories.map((story) => (
+                <div key={story.id}>
+                  <h2>{story.name} </h2>
+                  <p>{story.content}</p>
+                  <img src={story.imageUrl} height="100px"></img>
+                </div>
+              ))}
+          </div>
+        </div>
+      ) : (
+        "Loading"
+      )}
+    </>
   );
 }
